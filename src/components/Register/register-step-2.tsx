15da166b886dd43b2e4d9bbf76093065e7e54e74
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-03-13 10:23:47 +0330

    feat: register is done

---
+++ src/components/Register/register-step-2.tsx
@@ -0,0 +1,13 @@
+import CustomInput from "@FormFields/Initial/Input";
+
+const Register_Step_2 = () => {
+  return (
+    <CustomInput
+      label="Code Verification"
+      placeholder="Enter Verification Code"
+      type="number"
+    />
+  );
+};
+
+export default Register_Step_2;
