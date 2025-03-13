820dfb11168c552518970a2070a9a2e43cc99a45
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-03-13 11:54:43 +0330

    feat: add routes of my profile and edit profile

--- src/routes/ProfileRoutes.tsx
+++ src/routes/ProfileRoutes.tsx
@@ -2,6 +2,8 @@ import { RouteObject } from "react-router-dom";
 import ProfilePage from "../pages/ProfilePage";
 import ProfileDashboardPage from "../pages/ProfilePage/ProfileDashboardPage";
 import Test from "./../Test/Test";
+import MyProfilePage from "../pages/ProfilePage/MyProfilePage";
+import EditProfilePage from "../pages/ProfilePage/EditProfilePage";
 
 export const profileRoutes: RouteObject[] = [
   {
@@ -13,6 +15,14 @@ export const profileRoutes: RouteObject[] = [
       {
         path: "/profile/dashboard",
         element: <ProfileDashboardPage />,
+      },
+      {
+        path: "/profile/personal-info",
+        element: <MyProfilePage />,
+      },
+      {
+        path: "/profile/personal-info/edit",
+        element: <EditProfilePage />,
       },
       {
         path: "/profile/test",
