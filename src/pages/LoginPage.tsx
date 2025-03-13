d70cba2af96964be13d52e687911d7eba9144756
MoeinGanji2568 <146331540+MoeinGanji2568@users.noreply.github.com>
2025-03-13 10:21:20 +0330

    fix: change title of login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import { useChangeTitle } from "@hooks/useChangeTitle";
 import Login from "../components/Login";
 
 function LoginPage() {
-  useChangeTitle("ورود");
+  useChangeTitle("Login");
   return (
     <div>
       <Login />
