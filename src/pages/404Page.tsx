5b81a1255ff5708d07f5367c9c3bc66457c11b5d
Mohammad Badangiz <mbz3a1998@gmail.com>
2025-03-26 13:42:23 +0330

    feat: update 404 page text to English for improved accessibility and user experience

--- src/pages/404Page.tsx
+++ src/pages/404Page.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { useChangeTitle } from "@hooks/useChangeTitle";
 
 function Page404() {
-  useChangeTitle("صفحه ی مورد نظر یافت نشد.");
+  useChangeTitle("Page Not Found");
 
   const [counter, setCounter] = useState(30);
   const navigate = useNavigate();
@@ -25,23 +25,23 @@ function Page404() {
     <div className="flex h-svh w-full select-none content-center items-center justify-center">
       <div className="flex w-11/12 flex-col content-center items-center justify-evenly rounded-lg bg-white px-2 py-10 shadow-xl shadow-[rgba(0,0,0,0.02)] md:flex-row md:flex-wrap lg:h-80 lg:w-[900px] lg:py-9">
         <div className="space-y-5 text-center md:text-right">
-          <p className="font-Black_ir lg:text-xl">صفحه ی مورد نظر یافت نشد.</p>
+          <p className="font-Black_ir lg:text-xl">Page Not Found</p>
           <p className="text-sm text-light-steelBlue md:text-sm lg:text-15">
-            ** اگر از وجود صفحه ی مورد نظر اطمینان دارید به
+            ** If you're certain this page exists, please contact
             <Link
               to={""}
               className="mx-1 text-sm text-light-navyblue/70 hover:underline"
             >
-              پشتبیانی
+              support
             </Link>
-            نرم افزار اطلاع دهید.
+            .
           </p>
         </div>
         <div className="mt-8 animate-bounce text-[100px] md:text-[120px] lg:mt-0 lg:text-[150px]">
-          <p className="-rotate-90 font-Black_ir">:(</p>
+          <p className="font-Black_ir -rotate-90">:(</p>
         </div>
-        <p className="w-full text-center text-[10px] text-crimsonRed opacity-50 md:text-[11px] lg:text-sm">
-          شما تا {counter} ثانیه آینده به صفحه نخست منتقل می شوید.
+        <p className="text-crimsonRed w-full text-center text-[10px] opacity-50 md:text-[11px] lg:text-sm">
+          You will be redirected to the homepage in {counter} seconds.
         </p>
       </div>
     </div>
